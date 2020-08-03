58a146394e44e09dcfc16eabac6484dfe3a08fe6
Aslam <aslam17@navgurukul.org>
2020-08-03 18:28:32 +0530

    write a function for delete company
    write a function for delete company and its data

--- app/handlers/__company__handler.js
+++ app/handlers/__company__handler.js
@@ -82,4 +82,16 @@ exports.deleteTags = async (request, h) => {
 	})
 };
 
+exports.deleteCompany = async (request, h) => {
+	return new Promise((resolve, reject) =>{
+		companyModel.deleteOne({_id: request.query._id}, async function(err, doc){
+			if (err) {
+				return reject(Boom.forbidden(err))
+			}else{
+				return resolve(h.response({status: 'ok', documents: doc}))
+			}
+		})
+	})
+};
+
 
