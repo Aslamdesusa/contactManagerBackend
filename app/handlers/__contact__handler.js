14ce40bc73c5828f957bf07e71bf5b8282f33766
Aslam <aslam17@navgurukul.org>
2020-08-04 12:44:24 +0530

    Create __contact_handler.js
    created a module which can handler payload requests and save it to database

---
+++ app/handlers/__contact__handler.js
@@ -0,0 +1,19 @@
+// ** requiring company model **
+const contactModel = require('../models/__contact__model');
+
+const Boom = require('boom');
+
+// Creating Company Details 
+exports.createContact = async (request, h) => {
+	let pr = async (resolve, reject) => {
+		let new_contact = new contactModel(request.payload);
+		new_contact.save(async function(err, doc) {
+			if (err) {
+				return reject(Boom.forbidden(err));
+			} else {
+				return resolve(h.response({ status: 'ok', contactId: doc._id }).code(201));
+			}
+		});
+	};
+	return new Promise(pr);
+};
