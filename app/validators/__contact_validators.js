8b68642d61a052314106d22ae95c0069746d4814
Aslam <aslam17@navgurukul.org>
2020-08-04 12:55:55 +0530

    Update __contact_validators.js
    export a module to validate contact information with ( joi )

---
+++ app/validators/__contact_validators.js
@@ -0,0 +1,38 @@
+
+// Joi for validation
+const Joi = require('joi');
+
+// exporting module for contact to validate it's data
+module.exports = {
+	contactValidate: Joi.object().keys({
+        userId: Joi.string().required().trim().description('User Id Who Createing This contact'),
+        contactName: Joi.string().required().trim().description('contact name'),
+        title: Joi.string().allow('', null).trim().description('titler'),
+        description: Joi.string().allow('', null).trim().description('Description about company'),
+        tags: Joi.array().allow('', null).description('Any Tag For search this data'),
+        contactInfo: {
+            // contact information
+            email: Joi.string().allow('', null).trim().description('contact email'),
+            mobile: Joi.string().allow('', null).trim().description('contact mobile'),
+            workPhone: Joi.string().allow('', null).trim().description('contact work phone'),
+            homePhone: Joi.string().allow('', null).trim().description('contact homePhone')
+        },
+        address: {
+            // mailing address
+            mailingStreet: Joi.string().allow('', null).trim().description('mailing Street'),
+            mailingCity: Joi.string().allow('', null).trim().description('mailing City'),
+            mailingState: Joi.string().allow('', null).trim().description('mailing State'),
+            mailingCountry: Joi.string().allow('', null).trim().description('mailing Country'),
+            mailingZipCode: Joi.string().allow('', null).trim().description('mailing Zip Code')
+        },
+        avatarUrl: Joi.string().allow('', null).trim().description('Avtar Url'),
+    }),
+
+    _id: Joi.object().keys({
+		_id: Joi.string().required().trim().description('company id')
+    }),
+
+    tags: Joi.object().keys({
+		  tags: Joi.array().description('company tags to filter data')
+    }),
+};
