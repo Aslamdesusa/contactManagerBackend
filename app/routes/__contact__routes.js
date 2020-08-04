5680c9e60e05190a8bb1d940cc588ca345392062
Aslam <aslam17@navgurukul.org>
2020-08-04 13:04:52 +0530

    Create __contact__routes.js
    created _contact__routes.js file
    exports plugin where it can handle all the routes

---
+++ app/routes/__contact__routes.js
@@ -0,0 +1,18 @@
+exports.plugin = {
+	async register(server, options) {
+		const Controllers = {
+			contacts: {
+				contact: require('../controllers/__contact__controllers')
+			}
+        };
+        server.route([
+			{
+				method: 'POST',
+				path: '/create/contact',
+				options: Controllers.contacts.contact.createContact
+			},
+		]);
+	},
+	version: require('../../package.json').version,
+	name: 'contact_routes'
+};
