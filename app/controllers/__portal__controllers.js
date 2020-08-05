3c21a6d240c50a57483206fe90ec58bdcc945269
Aslam <aslam17@navgurukul.org>
2020-08-05 18:49:20 +0530

    commit middleware

--- app/controllers/__portal__controllers.js
+++ app/controllers/__portal__controllers.js
@@ -1,6 +1,6 @@
 const Handlers = require('../handlers/__portal__handler');
 const validators = require('../validators/__portal__validators');
-const middleware = require('../middlewars/portal_middlewars');
+// const middleware = require('../middlewars/portal_middlewars');
 
 // POST Contact Details
 exports.createPortal = {
@@ -9,6 +9,6 @@ exports.createPortal = {
 	validate: {
 		payload: validators.portalValidate
 	},
-	pre: [ { method: middleware.checkPortalExistance, assign: 'Portal' } ],
+	// pre: [ { method: middleware.checkPortalExistance, assign: 'Portal' } ],
 	handler: Handlers.createPortal,
 };
