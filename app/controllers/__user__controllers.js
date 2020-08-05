fb3f913be54bbaf31689b117bc0c2ca1e7622227
Aslam <aslam17@navgurukul.org>
2020-08-05 12:20:22 +0530

    Update __user__controllers.js

--- app/controllers/__user__controllers.js
+++ app/controllers/__user__controllers.js
@@ -1,5 +1,6 @@
 const Handlers = require('../handlers/__user__handler');
 const validators = require('../validators/__user_validators');
+const middleware = require('../middlewars/user_middlwars')
 
 // POST Contact Details
 exports.createUser = {
@@ -8,5 +9,6 @@ exports.createUser = {
 	validate: {
 		payload: validators.userValidate
     },
+    pre: [ { method: middleware.checkUserExistance, assign: 'User' } ],
 	handler: Handlers.createUser,
 };
