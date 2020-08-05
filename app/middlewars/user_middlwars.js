c4a9219377bd0e073f9e3e4779e308c0bf4dae85
Aslam <aslam17@navgurukul.org>
2020-08-05 18:56:11 +0530

    change

--- app/middlewars/user_middlwars.js
+++ app/middlewars/user_middlwars.js
@@ -1,6 +1,6 @@
 //model
 const userModel = require('../models/__user__model');
-const portalModel = require('../models/__portal__model')
+// const portalModel = require('../models/__portal__model')
 //helper
 const errorHelper = require('../helpers/error_helper');
 
@@ -12,15 +12,15 @@ exports.checkUserExistance = async (request, h) => {
 	try {
 		// if company exist'
 		let ifUserExist = await userModel.findOne({email: request.payload.email});
-		let ifPortalExist = await portalModel.findOne({portalName: request.payload.portalName});
+		// let ifPortalExist = await portalModel.findOne({portalName: request.payload.portalName});
 
 		if (ifUserExist) {
 			return Boom.conflict('User already exist.');
 		}
 
-		if (ifPortalExist) {
-			return Boom.conflict('Portal already exist.');
-		}
+		// if (ifPortalExist) {
+		// 	return Boom.conflict('Portal already exist.');
+		// }
 		return true;
 	} catch (err) {
 		errorHelper.handleError(err);
