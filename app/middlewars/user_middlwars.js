516cd1097d7ef77205114d023c8d786b59c811cd
Aslam <aslam17@navgurukul.org>
2020-08-05 12:11:01 +0530

    user existance function is ready
    This function will check each users of existance while sign-up if they would exist it will return  boom error with conflict otherwise it will be saved

---
+++ app/middlewars/user_middlwars.js
@@ -0,0 +1,21 @@
+//model
+const userModel = require('../models/__user__model');
+//helper
+const errorHelper = require('../helpers/error_helper');
+
+//dependencies
+const Boom = require('boom');
+
+exports.checkUserExistance = async (request, h) => {
+	try {
+		// if company exist'
+		let ifUserExist = await userModel.findOne({email: request.payload.email});
+		if (ifUserExist) {
+			return Boom.conflict('User already exist.');
+		}
+
+		return true;
+	} catch (err) {
+		errorHelper.handleError(err);
+	}
+};
