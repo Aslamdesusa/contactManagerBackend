27caf310fc753c03a3887c16e5787ce7cd7a2b4c
Aslam <aslam17@navgurukul.org>
2020-08-05 18:28:20 +0530

    changed on portal

--- app/controllers/__portal__controllers.js
+++ app/controllers/__portal__controllers.js
@@ -1,6 +1,6 @@
 const Handlers = require('../handlers/__portal__handler');
 const validators = require('../validators/__portal__validators');
-// const middleware = require('../middlewars/')
+const middleware = require('../middlewars/portal_middlewars')
 
 // POST Contact Details
 exports.createPortal = {
@@ -8,6 +8,7 @@ exports.createPortal = {
 	notes: 'CREATE portal Data',
 	validate: {
 		payload: validators.portalValidate
-    },
+	},
+	pre: [ { method: middleware.checkPortalExistance, assign: 'Portal' } ],
 	handler: Handlers.createPortal,
 };
--- app/middlewars/user_middlwars.js
+++ app/middlewars/user_middlwars.js
@@ -1,5 +1,6 @@
 //model
 const userModel = require('../models/__user__model');
+const portalModel = require('../models/__portal__model')
 //helper
 const errorHelper = require('../helpers/error_helper');
 
@@ -11,8 +12,14 @@ exports.checkUserExistance = async (request, h) => {
 	try {
 		// if company exist'
 		let ifUserExist = await userModel.findOne({email: request.payload.email});
+		let ifPortalExist = await portalModel.findOne({portalName: request.payload.portalName});
+
 		if (ifUserExist) {
 			return Boom.conflict('User already exist.');
+		}
+
+		if (ifPortalExist) {
+			return Boom.conflict('Portal already exist.');
 		}
 
 		return true;
