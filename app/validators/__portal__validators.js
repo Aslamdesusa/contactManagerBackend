78e3605e871aadffd8ddb80839a616ddcd961942
Aslam <aslam17@navgurukul.org>
2020-08-05 17:56:05 +0530

    node: joi validation on portal model

---
+++ app/validators/__portal__validators.js
@@ -0,0 +1,28 @@
+
+// Joi for validation
+const Joi = require('joi');
+
+// exporting module for contact to validate it's data
+module.exports = {
+	portalValidate: Joi.object().keys({
+        portalName: Joi.string().required().trim().description('portal name'),
+        portalUsers: [
+            {
+                userId: Joi.string().required().trim().description('userId'),
+                profile: Joi.string().required().trim().description('profile'),
+                rols: Joi.string().required().trim().description('rols'),
+                status: Joi.string().required().trim().description('status'),
+            }
+        ],
+        createdBy: {
+            userId: Joi.string().required().trim().description('userId'),
+            profile: Joi.string().required().trim().description('profile'),
+            rols: Joi.string().required().trim().description('rols'),
+            status: Joi.string().required().trim().description('status'),
+        },
+    }),
+
+    _id: Joi.object().keys({
+		_id: Joi.string().required().trim().description('user _id')
+    }),
+};
