9f32717c1b87936f2724f38d4fed16613912a8fc
Aslam <aslam17@navgurukul.org>
2020-08-05 18:54:14 +0530

    added middlewars again

--- app/controllers/__portal__controllers.js
+++ app/controllers/__portal__controllers.js
@@ -1,14 +1,14 @@
 const Handlers = require('../handlers/__portal__handler');
 const validators = require('../validators/__portal__validators');
-// const middleware = require('../middlewars/portal_middlewars');
+const middleware = require('../middlewars/portal_middlewars');
 
-// POST Contact Details
+// POST Portal Details
 exports.createPortal = {
 	tags: [ 'api' ],
 	notes: 'CREATE portal Data',
 	validate: {
 		payload: validators.portalValidate
 	},
-	// pre: [ { method: middleware.checkPortalExistance, assign: 'Portal' } ],
+	pre: [ { method: middleware.checkPortalExistance, assign: 'Portals' } ],
 	handler: Handlers.createPortal,
 };
