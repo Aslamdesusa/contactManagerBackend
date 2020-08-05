862b757852dbb155b5bfb68301fef31705575012
Aslam <aslam17@navgurukul.org>
2020-08-05 19:03:36 +0530

    complete

--- app/controllers/__portal__controllers.js
+++ app/controllers/__portal__controllers.js
@@ -1,6 +1,6 @@
 const Handlers = require('../handlers/__portal__handler');
 const validators = require('../validators/__portal__validators');
-const middleware = require('../middlewars/portals_middlewars');
+const middleware = require('../middlewars/user_middlwars');
 
 // POST Portal Details
 exports.createPortal = {
@@ -9,6 +9,6 @@ exports.createPortal = {
 	validate: {
 		payload: validators.portalValidate
 	},
-	pre: [ { method: middleware.checkPortalExistance, assign: 'Portals' } ],
+	pre: [ { method: middleware.checkUserExistance, assign: 'Portals' } ],
 	handler: Handlers.createPortal,
 };
